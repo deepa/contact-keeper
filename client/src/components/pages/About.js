881ba947a6d51e2e06f6d74871fb98d0ce5a2daa
Deepalakshmi Thiagarajan <deepa.name@gmail.com>
2019-07-08 02:05:33 -0700

    client project - intial commit

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className='my-1'>
+        This is a full stack React app for keeping contacts
+      </p>
+    </div>
+  );
+};
+
+export default About;
