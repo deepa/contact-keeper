e3b23f3a46068c97aa7c451bce1042e1138a2b9d
Deepalakshmi Thiagarajan <deepa.name@gmail.com>
2019-07-07 23:26:02 -0700

    register user and login flows

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect DB
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactKeeper API' }));
 
